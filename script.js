7743f83f4c39a46e6c57ef1be5639506de2b22cc
Lucia <lombardi074@gmail.com>
2024-04-12 19:23:59 +0200

    result page

--- script.js
+++ script.js
@@ -97,3 +97,100 @@
         incorrect_answers: ["Python", "C", "Jakarta"],
       },
     ];
+
+    // Funzione java script per controllare la risposta selezionatae 
+//+ 1 contatore risposte giuste se risposta giusta
+//+ 1 contatore risposte sbagliate se risposta sbagliata
+//+ 1 contatore risposte mancate se riposta non data
+
+
+//inizializzazione dei contatori 
+let rightAnswer = 0
+let falseAnswer = 0
+let missAnswer = 0
+
+//funzione per controllare la risposta 
+function controllaRisposta(rispostaData, rispostaCorretta) {
+    if(rispostaData === null || rispostaData === '') {
+       // nessuna risposta data
+       missAnswer += 1
+    } else if (rispostaData === rispostaCorretta) {
+        // Risposta giusta
+        rightAnswer +=1
+    } else {
+        //Risposta sbagliata
+        falseAnswer += 1
+
+    }
+
+    }
+
+
+  
+
+    // Aggiungere nell`HTML con l'id timer per visualizzare il countdown, come <div id="timer"></div>
+
+
+   //Funzione per passare alla prossima domanda (che si attiva sia su timer a 0, sia su risposta data) e passare alla pagina del punteggio finale, se abbiamo finito le domande
+
+   let indiceDomandaCorrente = 0; // Indice della domanda corrente
+   const domande = [] // Array delle  domande
+   let timerId //variabile per tenere traccia del timer 
+   let TimerIddiv = document.getElementById ("timer")
+   let durata = 30 
+
+   function avviaTimer (durata) {
+    clearInterval (timerId) //resetta il timer esistente
+
+    let tempoRimasto = durata
+    TimerIddiv.innerText = 'Tempo rimasto: ${tempoRimasto}s'
+
+
+   /* timerId = setInterval(() => {tempoRimasto--
+    Document.getElementById('timer').textContent = 'Tempo rimasto: ${tempoRimasto}s'
+    
+    if (tempoRimasto <= 0) {
+      clearInterval(timerId)
+      passaAllaDomandaSuccesiva()
+    }
+    
+  },1000)
+  */
+ 
+   }
+   
+   function passaAllaDomandaSuccesiva(){
+    //logica per passare alla prossima domanda 
+    console.log ('Passaggioa alla prossima Domanda')
+   }
+
+   //avviare il timer con 30 secondi
+   avviaTimer(30)
+
+// Funzione per prendere i risultati del quiz dal localStorage 
+function storeQuizResults(totaleDomande, corrette, saltate) {
+  localStorage.setItem('totaleDomande', totaleDomande);
+  localStorage.setItem('corrette', corrette);
+  localStorage.setItem('saltate', saltate); // Aggiungi anche le risposte saltate
+}
+
+// Funzione per calcolare e far apparire nel console log le risposte corrette, incorrette e saltate
+function calculateAndDisplayResults() {
+  const totaleDomande = parseInt(localStorage.getItem('totaleDomande'));
+  const corrette = parseInt(localStorage.getItem('corrette'));
+  const saltate = parseInt(localStorage.getItem('saltate'));
+  
+  const correctPercentage = (corrette / totaleDomande) * 100;
+  const incorrectPercentage = ((totaleDomande - corrette - saltate) / totaleDomande) * 100;
+  const skippedPercentage = (saltate / totaleDomande) * 100; 
+ 
+  
+  document.getElementById(`risultatoGiuste).innertext = 'La percentuale è: ${correctPercentage.toFixed(2)}%`);
+  document.getElementById(`risultatoSbagliate).innertext = 'La percentuale è: ${correctPercentage.toFixed(2)}%`);
+  document.getElementById(`risultatoSaltate).innertext = 'La percentuale è: ${correctPercentage.toFixed(2)}%`);
+
+}
+
+
+calculateAndDisplayResults(); // Calcolare e far apparire sul display i risultati
+
